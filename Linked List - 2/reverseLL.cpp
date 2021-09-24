cb355a28b38bd7112659ef6cab89fb6a3e7a55da
Pratham Vasani <73855805+JoKeR-VIKING@users.noreply.github.com>
2021-09-24 11:45:27 +0530

    Update reverseLL.cpp

--- Linked List - 2/reverseLL.cpp
+++ Linked List - 2/reverseLL.cpp
@@ -1,3 +1,5 @@
+// Recursive
+
 Node *reverseLinkedListRec(Node *head)
 {
     if (head == NULL || head -> next == NULL)
@@ -9,4 +11,26 @@ Node *reverseLinkedListRec(Node *head)
     head -> next = NULL;
     
     return temp;
+}
+
+// Iterative
+Node *reverseLinkedList(Node *head)
+{
+    if (head == NULL)
+        return head;
+    
+    Node* current = head;
+    Node* prev = NULL;
+    Node* next = head -> next;
+    
+    while (next != NULL)
+    {
+        current -> next = prev;
+        prev = current;
+        current = next;
+        next = next -> next;
+    }
+    
+    current -> next = prev;
+    return current;
 }
